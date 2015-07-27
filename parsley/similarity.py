37e633ecbf2fa7311eabc833ac51c6efdb133f2c
Ismail Elouafiq <ismail.elouafiq@telecom-bretagne.eu>
2015-07-27 12:03:44 +0200

    started similarity measures

--- parsley/similarity.py
+++ parsley/similarity.py
@@ -7,8 +7,24 @@ Considering strings as time series.
 @author: ielouafiq
 """
 
-def apca(firstSeries, secondSeries):
-	return 0
+class SimilarityMeasure(object):
+	# Similarity measure generation factory
+    def factory(similarity_measure_type):
+        #return eval(type + "()")
+        if similarity_measure_type == "APCA": return APCA()
+        if similarity_measure_type == "SAX" : return SAX()
+        assert 0, "Given similarity measure type is not handled: " + similarity_measure_type
+    factory = staticmethod(factory)
+		
+
+class APCA(SimilarityMeasure):
+	def __init__(self):
+		pass
+	def computeDistance(first_series, second_series):
+		return 0
 	
-def sax(firstSeries, secondSeries):
-	return 0
+class SAX(SimilarityMeasure):
+	def __init__(self):
+		pass
+	def computeDistance(first_series, second_series):
+		return 0
